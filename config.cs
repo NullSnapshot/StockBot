05df4482f0f1aa1f6e23b7439a39edd040a0572b
Trevor Naze <trevornaze@gmail.com>
2020-01-22 15:57:44 -0800

    Add configuration file validation

--- config.cs
+++ config.cs
@@ -10,22 +10,25 @@ namespace BotConfig
 {
     public static class Config
     {
-        public static string BotToken { get; private set; } = "";
+
+        //config filepath
         private const string CONFIG_FILEPATH = @"config/config.yml";
+
+        private static FileStream configFile;
+
+        //config values
+        private static int ConfigVersion = 1;
+        public static string BotToken { get; private set; } = "";
+        public static char CommandPrefix { get; private set; } = '!';
 
         static Config()
         {
-            FileStream configFile = new FileStream(CONFIG_FILEPATH, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            configFile = new FileStream(CONFIG_FILEPATH, FileMode.OpenOrCreate, FileAccess.ReadWrite);
 
             //empty file
             if (configFile.Length == 0)
             {
-                string configFileString = "";
-                StreamWriter sw = new StreamWriter(configFile);
-                configFileString = Serialize();
-                Console.WriteLine(configFileString);
-                sw.WriteLine(configFileString);
-                sw.Close();
+                WriteFile();
             }
             //read file
             else
@@ -33,8 +36,13 @@ namespace BotConfig
                 string configFileString = "";
                 StreamReader sr = new StreamReader(configFile);
                 configFileString = sr.ReadToEnd();
-                DeSerialize(configFileString);
+                bool fileBuildNeeded = DeSerialize(configFileString);
                 sr.Close();
+                if (fileBuildNeeded)
+                {
+                    Console.WriteLine("Building new config file");
+                    WriteFile();
+                }
             }
             configFile.Close();
         }
@@ -44,7 +52,9 @@ namespace BotConfig
             YamlStream yaml = new YamlStream(
                 new YamlDocument(
                     new YamlMappingNode(
-                        new YamlScalarNode("BotToken"), new YamlScalarNode(BotToken)
+                        new YamlScalarNode("ConfigVersion"), new YamlScalarNode(ConfigVersion.ToString()),
+                        new YamlScalarNode("BotToken"), new YamlScalarNode(BotToken),
+                        new YamlScalarNode("CommandPrefix"), new YamlScalarNode(CommandPrefix.ToString())
                     )
                 )
             );
@@ -55,12 +65,66 @@ namespace BotConfig
             return sw.ToString();
         }
 
-        private static void DeSerialize(string configString)
+        private static bool DeSerialize(string configString)
         {
             Deserializer de = new Deserializer();
             Dictionary<string, string> configs = de.Deserialize<Dictionary<string, string>>(configString);
+            int tempConfigVersion = -1;
+            string tempversionString = "";
 
-            BotToken = configs["BotToken"];
+            //attempt to read config file version
+            bool configInvalid = !configs.TryGetValue("ConfigVersion", out tempversionString);
+
+            if (configInvalid) //config file does not contain version param.
+            {
+                Console.WriteLine($@"Config file does not specify version.
+                    Expected Version: {ConfigVersion}");
+            }
+            else //config file contains version param. Attempt to validate latest version.
+            {
+                configInvalid |= !Int32.TryParse(tempversionString, out tempConfigVersion);
+                if (configInvalid)
+                {
+                    Console.WriteLine($@"Unable to parse config file version.
+                        Expected Version: {ConfigVersion}");
+                }
+            }
+            if (tempConfigVersion < ConfigVersion) //file out of date.
+            {
+                configInvalid = true;
+                Console.WriteLine($@"Config File Version out of Date. 
+                    Current Version: {tempConfigVersion} | 
+                    Expected Version: {ConfigVersion}");
+            }
+
+            if (!validateFilePair(configs, "BotToken")) configInvalid = true;
+            else BotToken = configs["BotToken"];
+
+            if (!validateFilePair(configs, "CommandPrefix")) configInvalid = true;
+            else CommandPrefix = configs["CommandPrefix"].ToCharArray()[0];
+
+
+
+            //return true if file build is necessary.
+            return configInvalid;
+        }
+
+        private static void WriteFile()
+        {
+            FileStream configFile = new FileStream(CONFIG_FILEPATH, FileMode.Create, FileAccess.Write);
+            string configFileString = "";
+            StreamWriter sw = new StreamWriter(configFile);
+            configFileString = Serialize();
+            sw.WriteLine(configFileString);
+            sw.Close();
+        }
+
+        private static bool validateFilePair(Dictionary<string, string> dictPool, string key)
+        {
+            string keyValue = "";
+            bool valid = dictPool.TryGetValue(key, out keyValue);
+            if (String.IsNullOrEmpty(keyValue)) return false;
+            return valid;
         }
     }
 }
